402caf5a25554bc4b7eeca3c20450fb36163440f
Rahimeen Altaf <abbasirahimeen@gmail.com>
2024-08-31 03:05:33 +0500

    check username unique

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,22 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
